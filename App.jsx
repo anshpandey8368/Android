16a81682e59ea995381e3812f8b22e4ab35d7c1d
Ansh Pandey <anshpandey8368@gmail.com>
2025-01-06 11:14:03 +0530

    Increase count

--- App.jsx
+++ App.jsx
@@ -1,12 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View , SafeAreaView, Button, TouchableOpacity} from 'react-native'
 import React from 'react'
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text style = {styles.text}>Ansh Pandey</Text>
+  const [count, setCount] = React.useState(0)
 
-    </View>
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Tanish ki gaand mei kitne lode hai ?</Text>
+      <Text style={styles.text1}>{count}</Text>
+      <Image style={styles.img} source={require('./2.jpg')} />
+      <TouchableOpacity onPress={() => setCount(count + 1)}>
+        <Text style={styles.btn}>Click</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   )
 }
 
@@ -18,11 +24,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 20,
   },
   text: {
     fontSize: 20,
     fontWeight: 'bold',
-    color: 'red',
-    backgroundColor: 'yellow'
+    color: 'white',
+    backgroundColor: 'black'
+  },
+  text1: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  img: {
+    width: 200,
+    height: 200,
+  },
+  btn: {
+    backgroundColor: 'black',
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 10,
+    borderRadius: 5
   }
 })
